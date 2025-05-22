3f8e81d2eaa50420ab5cb168dc64db6bf3db6a51
Johannes Schill <johannes.schill@gmail.com>
2025-05-22 09:27:32 +0200

    fix: Export server actions properly

--- web/src/app/(protected)/subscription/actions.ts
+++ web/src/app/(protected)/subscription/actions.ts
@@ -1,4 +1,14 @@
 "use server"
+import { createCheckoutSession as createCheckoutSessionAction } from '@/lib/stripe/server-actions/create-checkout-session'
+import { handleCheckoutSuccess as handleCheckoutSuccessAction } from '@/lib/stripe/server-actions/handle-checkout-success'
 
-export { createCheckoutSession } from '@/lib/stripe/server-actions/create-checkout-session'
-export { handleCheckoutSuccess } from '@/lib/stripe/server-actions/handle-checkout-success'
+// Can not export directly from the file, i get a "Can only export async functions from server actions" error.
+// Wrapping them in a function solves this.
+
+export async function createCheckoutSession(...props: Parameters<typeof createCheckoutSessionAction>) {
+  return createCheckoutSessionAction(...props)
+}
+
+export async function handleCheckoutSuccess(...props: Parameters<typeof handleCheckoutSuccessAction>) {
+  return handleCheckoutSuccessAction(...props)
+}
